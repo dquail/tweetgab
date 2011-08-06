ac5f9c32aac28139d325103f73a3d0c6354a44ca
dquail <quail.david@gmail.com>
2011-08-06 11:04:18 -0700

    Add project code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetgab::Application.config.secret_token = '50b86374f425a4e0cd8c4cf047775bc003d4e548cb622cca5e080cc54198d5418527bf458eb9f8ddc7fe54037fbb3eaab9783bcc9708c262e4f4c5976828ac05'
