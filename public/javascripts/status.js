9a31fd24013bf02ca20f1622863d3df15ce38429
Nathan Baxter <nathan@logicaldecay.com>
2011-08-07 12:20:28 -0700

    Backbone view/model/controller for statuses.

--- public/javascripts/status.js
+++ public/javascripts/status.js
@@ -1,2 +1,38 @@
+
 var Status = Backbone.Model.extend({
 });
+
+var Statuses = Backbone.Collection.extend({
+  model: Status
+});
+
+var StatusView = Backbone.View.extend({
+  initialize: function() {
+    $('#statuses').prepend(this.el);
+    this.render();
+  },
+  tagName: 'div',
+  className: 'status',
+  events: {
+    
+  },
+  render: function() {
+    $(this.el).html("This is a status rendered at " + new Date());
+    return this
+  }
+});
+
+var StatusController = {
+  initialize: function() {
+    StatusController.statuses.bind("add", this.add, this);
+  },
+  statuses: new Statuses(),
+  statusViews: [],
+  add: function(status) {
+    this.statusViews.push(new StatusView({
+      model: status,
+      id: "status-" + status.id
+    }));
+  }
+}
+StatusController.initialize();
